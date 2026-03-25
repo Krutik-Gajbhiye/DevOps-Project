ec831b7afe271f2ed62f6e5c1775ec37915b1a3d
Krutik-Gajbhiye <krutikgajbhiye@gmail.com>
2026-03-25 23:49:29 +0530

    Create routes/routes.js for consolidating all API routes

---
+++ routes/routes.js
@@ -0,0 +1,14 @@
+// Consolidating all API routes
+
+const express = require('express');
+const router = express.Router();
+
+// Import routes
+const userRoutes = require('./userRoutes');
+const productRoutes = require('./productRoutes');
+
+// Use routes
+router.use('/users', userRoutes);
+router.use('/products', productRoutes);
+
+module.exports = router;
